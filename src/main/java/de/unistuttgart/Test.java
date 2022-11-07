9c92886fa54f8ec1b574209c6315adcea6ab5abd
paula <stopfen.kuehlboxen_0w@icloud.com>
2022-11-07 17:57:01 +0100

    cleared test class

--- src/main/java/de/unistuttgart/Test.java
+++ src/main/java/de/unistuttgart/Test.java
@@ -14,18 +14,5 @@ public class Test {
         Graph graph = Graph.readGraphFromFile(pathname);
         Graph.printGraph(graph);
 
-        /*
-         List<Node> nodes = Arrays.asList(new Node(0), new Node(1), new Node(2),
-                new Node(3), new Node(4));
-        List<Edge> edges = Arrays.asList(
-                new Edge(0, 1, 9), new Edge(0, 2, 8), new Edge(0, 4, 7),
-                new Edge(2, 0, 6), new Edge(2, 1, 4), new Edge(2, 4, 4),
-                new Edge(3, 2, 3), new Edge(4, 1, 2), new Edge(4, 3, 1));
-
-        Graph graph = new Graph(5, 9, nodes, edges);
-
-        Graph.printGraph(graph);
-         */
-
     }
 }
