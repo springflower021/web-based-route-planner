495177ae6aa866f23980702f605a34b4db1e5388
paula <stopfen.kuehlboxen_0w@icloud.com>
2022-11-07 13:14:31 +0100

    Formated code

--- src/main/java/de/unistuttgart/Node.java
+++ src/main/java/de/unistuttgart/Node.java
@@ -1,31 +1,31 @@
 package de.unistuttgart;
 
 public class Node {
-	// nodeID
-	int id;
-	// nodeID2
-	int id2;
-	// latitude 
-	double lati;
-	// longitude
-	double longi;
-	// elevation
-	int elev;
-	
-	Node(int id) {
-		this.id = id;
-	}
-	
-	Node(int id, double lati, double longi) {
-		this.id = id;
-		this.lati = lati;
-		this.longi = longi;
-	}
-	
-	public int getId() {
-		// TODO Auto-generated method stub
-		return id;
-	}
-	
+    // nodeID
+    int id;
+    // nodeID2
+    int id2;
+    // latitude
+    double lati;
+    // longitude
+    double longi;
+    // elevation
+    int elev;
+
+    Node(int id) {
+        this.id = id;
+    }
+
+    Node(int id, double lati, double longi) {
+        this.id = id;
+        this.lati = lati;
+        this.longi = longi;
+    }
+
+    public int getId() {
+        // TODO Auto-generated method stub
+        return id;
+    }
+
 
 }
